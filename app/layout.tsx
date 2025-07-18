aa904bdf0a5f788b3b555e2504d5869ede5ec3c4
Night <hotcoding85@gmail.com>
2025-07-18 11:49:20 -0400

    fix: fixed build issue

--- app/layout.tsx
+++ app/layout.tsx
@@ -50,8 +50,8 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-[#15181a]`}
       >
         <WalletProvider>
-          {/* <PHProvider>
-            <PostHogPageview /> */}
+          <PHProvider>
+            <PostHogPageview />
             {/* <PostHogErrorTracker /> */}
             <ReduxProvider>
               <ThemeProvider
@@ -66,7 +66,7 @@ export default function RootLayout({
                 </LanguageProvider>
               </ThemeProvider>
             </ReduxProvider>
-          {/* </PHProvider> */}
+          </PHProvider>
         </WalletProvider>
       </body>
     </html>
